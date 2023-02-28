37159967b68e24b2e5320f86cfa664e5fc4dc347
Andrew <matveichev.ap@phystech.edu>
2023-02-28 17:37:29 +0300

    lab 2

--- 1/step 3/2.py
+++ 1/step 3/2.py
@@ -9,20 +9,14 @@ gmsh.initialize()
 path = os.path.dirname(os.path.abspath(__file__))
 gmsh.merge(os.path.join(path, 'phrog.stl'))
 
-gmsh.option.setNumber("General.Terminal", 1)
-gmsh.option.setNumber("Mesh.Algorithm", 8);
-gmsh.option.setNumber("Mesh.CharacteristicLengthMin", 1);
-gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 2);
-gmsh.option.setNumber("Mesh.Optimize", 2)
-gmsh.option.setNumber("Mesh.QualityType", 2);
-
 n = gmsh.model.getDimension()
 s = gmsh.model.getEntities(n)
 l = gmsh.model.geo.addSurfaceLoop([s[i][1] for i in range(len(s))])
+
 gmsh.model.geo.addVolume([l])
 print("Volume added")
-gmsh.model.geo.synchronize()
 
+gmsh.model.geo.synchronize()
 gmsh.model.mesh.generate(3)
 
 gmsh.write('t_frog.msh')
